a2c8080776a5ffdb642cc7e43f90ad519b29edd4
kkwongs <kihyun5741@gmail.com>
2021-01-02 19:05:31 +0900

    코드업 기초 100제(1038-1052)

---
+++ 코드업/기초 100제(1038-1046)[산술연산].py
@@ -0,0 +1,41 @@
+#1038
+a, b = map(int,input().split())
+print(a + b)
+
+#1039
+a, b = map(int,input().split())
+print(a + b)
+
+#1040
+n = int(input())
+print(-n)
+
+#1041
+n = input()
+print(chr(ord(n) + 1))
+
+#1042
+a, b = map(int,input().split())
+print(a // b)
+
+#1043
+a, b = map(int,input().split())
+print(a % b)
+
+#1044
+n = int(input())
+print(n + 1)
+
+#1045
+a, b = map(int,input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+print("%.2f" % float(a/b))
+
+#1046
+a, b, c = map(int,input().split())
+print(a+b+c)
+print("%.1f" % ((a+b+c)/3))
---
+++ 코드업/기초 100제(1047-1048)[비트시프트연산].py
@@ -0,0 +1,7 @@
+#1047
+n = int(input())
+print(n << 1)
+
+#1048
+a, b = map(int, (input().split()))
+print(a << b)
