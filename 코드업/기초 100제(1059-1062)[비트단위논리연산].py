53088fe89c9ec5cdaf87739ae9c543cbd9e66018
kkwongs <kihyun5741@gmail.com>
2021-01-04 23:47:24 +0900

    코드업 기초 100제(1059-1064)

---
+++ 코드업/기초 100제(1059-1062)[비트단위논리연산].py
@@ -0,0 +1,15 @@
+#1059
+n = int(input())
+print(~n)
+
+#1060
+a, b = map(int, input().split())
+print(a & b)
+
+#1061
+a, b = map(int, input().split())
+print(a | b)
+
+#1062
+a, b = map(int, input().split())
+print(a ^ b)
