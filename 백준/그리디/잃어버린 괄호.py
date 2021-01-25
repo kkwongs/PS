8667c7f40a021039d049f07c8ce32a9125934db4
kkwongs <kihyun5741@gmail.com>
2021-01-25 22:25:41 +0900

    #18 잃어비린 괄호

---
+++ 백준/그리디/잃어버린 괄호.py
@@ -0,0 +1,13 @@
+# https://www.acmicpc.net/problem/1541
+
+s = input().split('-')
+result = 0
+
+for i in s[0].split('+'):
+  result += int(i)
+
+for i in s[1:]:
+  for j in i.split('+'):
+    result -= int(j)
+
+print(result)
