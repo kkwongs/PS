f657c37b69f2234923393c94341e7930438a1f03
kkwongs <kihyun5741@gmail.com>
2021-09-12 23:47:47 +0900

    백준 - 기준 수학 2 (4948 improve)

--- 백준/기본 수학 2/베르트랑 공준.js
+++ 백준/기본 수학 2/베르트랑 공준.js
@@ -1,3 +1,4 @@
+/* first solution
 let input = require('fs')
   .readFileSync('/dev/stdin')
   .toString()
@@ -26,6 +27,41 @@ for (let i = 0; i < input.length - 1; i++) {
   let count = 0;
   for (let j = n + 1; j <= 2 * n; j++) {
     if (isPrime(j)) {
+      count++;
+    }
+  }
+
+  result.push(count);
+}
+
+console.log(result.join('\n'));
+*/
+
+// 에라토스테네스의 체
+let input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+// let input = ['1', '10', '13', '100', '1000', '10000', '100000', '0'];
+
+let n = 123456;
+let isPrime = Array(2 * n + 1).fill(true);
+let result = [];
+
+for (let i = 2; i * i <= 2 * n; i++) {
+  if (isPrime[i]) {
+    for (let j = i * i; j <= 2 * n; j += i) {
+      isPrime[j] = false;
+    }
+  }
+}
+
+for (let i = 0; i < input.length - 1; i++) {
+  n = Number(input[i]);
+  let count = 0;
+  for (let j = n + 1; j <= 2 * n; j++) {
+    if (isPrime[j]) {
       count++;
     }
   }
