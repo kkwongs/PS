0c159a983bdcc6377c9c954d1c8d2e8d474c694d
kkwongs <kihyun5741@gmail.com>
2021-11-20 23:30:34 +0900

    백준 - 카드 구매하기 (11052)

---
+++ 백준/400 - 다이나믹 프로그래밍 1/카드 구매하기.js
@@ -0,0 +1,18 @@
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+// const input = ['4', '1 5 6 7'];
+const N = Number(input.shift());
+const P = input.shift().split(' ').map(Number);
+P.unshift(0);
+const DP = new Array(N + 1).fill(0);
+
+for (let i = 1; i <= N; i++) {
+  for (let j = 1; j <= i; j++) {
+    DP[i] = Math.max(DP[i], DP[i - j] + P[j]);
+  }
+}
+
+console.log(DP[N]);
